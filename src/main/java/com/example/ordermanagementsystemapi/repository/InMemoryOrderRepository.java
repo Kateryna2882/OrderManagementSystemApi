9e1baba45efa16bd25871e466ca8cc66660db313
Kateryna2882 <govoryhina82@gmail.com>
2024-02-06 21:06:51 +0200

    Add methods in the package repository

--- src/main/java/com/example/ordermanagementsystemapi/repository/InMemoryOrderRepository.java
+++ src/main/java/com/example/ordermanagementsystemapi/repository/InMemoryOrderRepository.java
@@ -1,4 +1,35 @@
 package com.example.ordermanagementsystemapi.repository;
 
-public class InMemoryOrderRepository {
+
+import com.example.apiordermanagementsystem.model.Order;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+@Repository
+public class  InMemoryOrderRepository implements OrderRepository {
+
+    private final Map<Long, Order> orders = new HashMap<>();
+
+    @Override
+    public Optional<Order> findById(Long orderId) {
+
+        return Optional.ofNullable(orders.get(orderId));
+    }
+
+    @Override
+    public Order save(Order order) {
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    @Override
+    public void deleteById(Long orderId) {
+
+        orders.remove(orderId);
+    }
+
+
 }
--- src/main/java/com/example/ordermanagementsystemapi/repository/OrderRepository.java
+++ src/main/java/com/example/ordermanagementsystemapi/repository/OrderRepository.java
@@ -1,4 +1,14 @@
 package com.example.ordermanagementsystemapi.repository;
 
+import com.example.apiordermanagementsystem.model.Order;
+
+import java.util.Optional;
+
 public interface OrderRepository {
+    Optional<Order> findById(Long orderId);
+
+
+    Order save(Order order);
+
+    void deleteById(Long orderId);
 }
