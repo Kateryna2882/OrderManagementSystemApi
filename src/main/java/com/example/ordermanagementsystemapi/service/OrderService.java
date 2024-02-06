e082af75702285e8c3dd94d46212ebf687f206b3
Kateryna2882 <govoryhina82@gmail.com>
2024-02-06 20:55:32 +0200

    Add methods in the package service

--- src/main/java/com/example/ordermanagementsystemapi/service/OrderService.java
+++ src/main/java/com/example/ordermanagementsystemapi/service/OrderService.java
@@ -1,4 +1,61 @@
 package com.example.ordermanagementsystemapi.service;
 
+import com.example.apiordermanagementsystem.exception.OrderNotFoundException;
+import com.example.apiordermanagementsystem.model.Order;
+import com.example.apiordermanagementsystem.model.Product;
+import com.example.apiordermanagementsystem.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    @Autowired
+    public OrderService(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    public Order getOrderById(Long orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderNotFoundException(orderId));
+    }
+
+    public Order createOrder(Order order) {
+        return orderRepository.save(order);
+    }
+
+    public Order updateOrder(Long orderId, Order updatedOrder) {
+        getOrderByIdOrThrow(orderId);
+
+        updatedOrder.setId(orderId);
+        return orderRepository.save(updatedOrder);
+    }
+
+    public Order addProductToOrder(Long orderId, Product product) {
+        Order order = getOrderByIdOrThrow(orderId);
+
+        order.getProducts().add(product);
+        return orderRepository.save(order);
+    }
+
+    public void removeProductFromOrder(Long orderId, Long productId) {
+        Order order = getOrderByIdOrThrow(orderId);
+
+        order.getProducts().removeIf(product -> product.getId().equals(productId));
+        orderRepository.save(order);
+    }
+
+    public void deleteOrder(Long orderId) {
+        Order order = getOrderByIdOrThrow(orderId);
+
+        orderRepository.deleteById(order.getId());
+    }
+
+    private Order getOrderByIdOrThrow(Long orderId) {
+        return orderRepository.findById(orderId)
+                .orElseThrow(() -> new OrderNotFoundException(orderId));
+    }
 }
--- src/main/java/com/example/ordermanagementsystemapi/service/ProductService.java
+++ src/main/java/com/example/ordermanagementsystemapi/service/ProductService.java
@@ -1,4 +1,55 @@
 package com.example.ordermanagementsystemapi.service;
 
+import com.example.apiordermanagementsystem.exception.ProductNotFoundException;
+import com.example.apiordermanagementsystem.model.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
+@Service
 public class ProductService {
+
+    private final List<Product> products = new ArrayList<>();
+    private final AtomicLong productIdCounter = new AtomicLong(0);
+
+    public List<Product> getAllProducts() {
+        return products;
+    }
+
+    public Product getProductById(Long productId) {
+        return findProductById(productId).orElseThrow(() -> new ProductNotFoundException(productId));
+    }
+
+    public Product createProduct(Product product) {
+        product.setId(generateProductId());
+        products.add(product);
+        return product;
+    }
+
+    public Product updateProduct(Long productId, Product updatedProduct) {
+        Product existingProduct = getProductById(productId);
+
+        existingProduct.setName(updatedProduct.getName());
+        existingProduct.setPrice(updatedProduct.getPrice());
+
+        return existingProduct;
+    }
+
+    public void deleteProduct(Long productId) {
+        Product product = getProductById(productId);
+        products.remove(product);
+    }
+
+    private Optional<Product> findProductById(Long productId) {
+        return products.stream()
+                .filter(p -> p.getId().equals(productId))
+                .findFirst();
+    }
+
+    private Long generateProductId() {
+        return productIdCounter.incrementAndGet();
+    }
 }
