c1bf2dd9125df6617313514ea275fdf27b2e51bd
Kateryna2882 <govoryhina82@gmail.com>
2024-02-06 20:51:25 +0200

    Add methods in the package model

--- src/main/java/com/example/ordermanagementsystemapi/model/Order.java
+++ src/main/java/com/example/ordermanagementsystemapi/model/Order.java
@@ -1,4 +1,23 @@
 package com.example.ordermanagementsystemapi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+@Data
+@NoArgsConstructor
+@Entity
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+
 }
--- src/main/java/com/example/ordermanagementsystemapi/model/Product.java
+++ src/main/java/com/example/ordermanagementsystemapi/model/Product.java
@@ -1,4 +1,19 @@
 package com.example.ordermanagementsystemapi.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double price;
+
 }
