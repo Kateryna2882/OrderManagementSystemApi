24f1a13e683545cdf51cf554ef62285881dcbf77
Kateryna2882 <govoryhina82@gmail.com>
2024-02-06 20:42:49 +0200

    Add classes in the package repository

---
+++ src/main/java/com/example/ordermanagementsystemapi/repository/InMemoryOrderRepository.java
@@ -0,0 +1,4 @@
+package com.example.ordermanagementsystemapi.repository;
+
+public class InMemoryOrderRepository {
+}
---
+++ src/main/java/com/example/ordermanagementsystemapi/repository/OrderRepository.java
@@ -0,0 +1,4 @@
+package com.example.ordermanagementsystemapi.repository;
+
+public interface OrderRepository {
+}
