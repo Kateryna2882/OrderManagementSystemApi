a81607eeee8a2ac9eb469feb3d0199fa83e5ee2e
Kateryna2882 <govoryhina82@gmail.com>
2024-02-06 21:10:04 +0200

    Add methods in the package controller

--- src/main/java/com/example/ordermanagementsystemapi/controller/OrderController.java
+++ src/main/java/com/example/ordermanagementsystemapi/controller/OrderController.java
@@ -1,4 +1,94 @@
 package com.example.ordermanagementsystemapi.controller;
 
+import com.example.apiordermanagementsystem.exception.OrderNotFoundException;
+import com.example.apiordermanagementsystem.exception.ProductNotFoundException;
+import com.example.apiordermanagementsystem.model.Order;
+import com.example.apiordermanagementsystem.model.Product;
+import com.example.apiordermanagementsystem.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+@ControllerAdvice
+@Component
+@Controller
+@RequestMapping("/api/orders")
 public class OrderController {
-}
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order createdOrder = orderService.createOrder(order);
+        return new ResponseEntity<>(createdOrder, HttpStatus.CREATED);
+    }
+    @GetMapping("/{orderId}")
+    public ResponseEntity<Order> getOrder(@PathVariable Long orderId) {
+        try {
+            Order order = orderService.getOrderById(orderId);
+            return new ResponseEntity<>(order, HttpStatus.OK);
+        } catch (OrderNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            // Обробляйте інші виключення під час виконання або реєструйте їх
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+
+    @PutMapping("/{orderId}")
+    public ResponseEntity<Order> updateOrder(@PathVariable Long orderId, @RequestBody Order order) {
+        try {
+            Order updatedOrder = orderService.updateOrder(orderId, order);
+            return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
+        } catch (OrderNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            // Обробляйте інші виключення під час виконання або реєструйте їх
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping("/{orderId}/add-product")
+    public ResponseEntity<Order> addProductToOrder(@PathVariable Long orderId, @RequestBody Product product) {
+        try {
+            Order updatedOrder = orderService.addProductToOrder(orderId, product);
+            return new ResponseEntity<>(updatedOrder, HttpStatus.OK);
+        } catch (OrderNotFoundException | ProductNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (RuntimeException e) {
+            // Обробляйте інші виключення під час виконання або реєструйте їх
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{orderId}/remove-product/{productId}")
+    public ResponseEntity<Void> removeProductFromOrder(@PathVariable Long orderId, @PathVariable Long productId) {
+        try {
+            orderService.removeProductFromOrder(orderId, productId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (OrderNotFoundException | ProductNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (RuntimeException e) {
+
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{orderId}")
+    public ResponseEntity<Void> deleteOrder(@PathVariable Long orderId) {
+        try {
+            orderService.deleteOrder(orderId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (OrderNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (RuntimeException e) {
+            // Обробляйте інші виключення під час виконання або реєструйте їх
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
